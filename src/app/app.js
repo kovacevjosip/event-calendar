b250707ffb800bd7c16b7728edcdbc801f8366b3
kovacevic.josip <kovacevic.josip@nsoft.com>
2017-08-22 17:26:04 +0200

    Remove unnecesary state

--- src/app/app.js
+++ src/app/app.js
@@ -24,15 +24,8 @@ app.config(function (
 
     // Routes
     $stateProvider
-        .state('base', {
-            controller: 'BaseCtrl',
-            controllerAs: 'base',
-            templateUrl: 'app/components/base/view.html',
-            abstract: true
-        })
         .state('home', {
             url: '/',
-            parent: 'base',
             controller: 'HomeCtrl',
             controllerAs: 'home',
             templateUrl: 'app/components/home/view.html',
